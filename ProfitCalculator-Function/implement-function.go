607bb8d69b7731e3daef55418d83118d90a2151f
Irfando <irfandoaritonang86@gmail.com>
2023-12-17 17:24:13 +0700

    finish implementation function

---
+++ ProfitCalculator-Function/implement-function.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var revenue float64
+	// var expenses float64
+	// var taxRate float64
+
+ revenue :=  getUserInput("Revenue: ")
+ expenses :=  getUserInput("Expense: ")
+ taxRate :=  getUserInput("Tax Rate: ")
+
+ ebt, profit, ratio := Calculation(revenue, expenses, taxRate)
+
+
+
+	fmt.Println("ebt: ", ebt)
+	fmt.Println("profit: ", profit)
+	fmt.Println("ratio: ",ratio)
+}
+
+
+//implementasi function
+//ubah ke float64 karena ada bilangan desimal
+func getUserInput(catchInput string) float64  {
+	var userInput float64
+	fmt.Print(catchInput)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+
+//isi paramaternya dan tipe data nya
+//karena mau mengembalikan float tambahkan () untuk menerima nilai kembalian/ouput
+//setelah itu tambahan return untuk mencetak
+func Calculation(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := 	revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
